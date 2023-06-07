8dd5ae481d81c9b60bcd3a75963f4767b6781d4c
LeMinhTuong <leminhtuong09122002@gmail.com>
2023-06-08 02:55:09 +0700

    test auto deploy

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -49,7 +49,7 @@ const Navbar = () => {
 
       <ul className={`${styles.navbar} ${showNavbar ? styles.show : ""}`}>
         <span className={styles.navbar_content}>
-          JavaScript Documentation
+          JS Documentation
           <div className={styles.close_icon}>
             <CloseIcon
               style={{ fontSize: "2rem" }}
